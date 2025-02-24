b5ea2f14d3081a4d054fef19787c138a463cefd6
AmjadxDev <AmjadxDev@gmail.com>
2025-02-24 11:40:12 +0500

    React Flow and structure

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './Chai';
+
+
+function App() {
+  return (
+    <>
+    <h1>My React App</h1>
+    <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './Chai.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite React</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
